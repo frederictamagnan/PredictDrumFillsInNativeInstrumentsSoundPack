c1f9993fc70bef5c639353b400773a8659c64235
frederic tamagnan <frederic.tamagnan@gmail.com>
2019-02-09 22:16:50 +0800

    debubg labelling

--- cleanLabeling/ScriptBuildDatasetGenerationServer.py
+++ cleanLabeling/ScriptBuildDatasetGenerationServer.py
@@ -95,6 +95,7 @@ def build_generation_dataset(p, npz):
     label = label['label']
     metadata_dict = dict(np.load(p + '/' + npz.replace('_label','_metadata_training')))
     vae=metadata_dict['vae_embeddings']
+    print(len(label),"LABEL",len(vae),"VAE")
     #     print(label.shape)
     print(p + '/' + npz.replace('_label.npz', ''))
     multi = Multitrack(p + '/' + npz.replace('_label', ''))
