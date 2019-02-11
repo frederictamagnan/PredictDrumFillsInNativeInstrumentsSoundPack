37503e439513b3fc5f5d49ab526a5e81f67b43b9
frederic tamagnan <frederic.tamagnan@gmail.com>
2019-02-11 11:52:37 +0800

    labelling 0.7

--- drumGeneration/ScriptExploreFills.py
+++ drumGeneration/ScriptExploreFills.py
@@ -10,7 +10,7 @@ from utils import numpy_drums_save_to_midi
 # # print(vae[:10])
 # print(genre.shape,vae.shape,genre.sum())
 
-data=np.load('/home/ftamagna/Documents/_AcademiaSinica/dataset/drumGeneration/reduced_fills_plus_embeddings.npz')
+data=np.load('/home/ftamagna/Documents/_AcademiaSinica/dataset/drumGeneration/reduced_fills_plus_embeddings1102.npz')
 data=dict(data)
 for elt in data.keys():
     print(data[elt].shape)
@@ -19,7 +19,7 @@ tr=data['track_array']
 from DrumReducerExpander import DrumReducerExpander
 dec=DrumReducerExpander()
 for i in range(len(tr)):
-    track=tr[i].reshape((3*16,9))
+    track=tr[i+100].reshape((4*16,9))
     track=dec.decode(batch_pianoroll=track,no_batch=True)
     track=dec.decode_808(batch_pianoroll=track,no_batch=True)
     numpy_drums_save_to_midi(track,'/home/ftamagna/Documents/_AcademiaSinica/dataset/temp/',str(i))
