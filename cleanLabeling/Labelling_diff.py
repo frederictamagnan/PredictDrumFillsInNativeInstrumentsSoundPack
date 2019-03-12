14500699a6bb79003cd4f04d9125bfcfcb9faa92
frederic tamagnan <frederic.tamagnan@gmail.com>
2019-03-12 17:48:12 +0800

    labelling diff

--- cleanLabeling/Labelling_diff.py
+++ cleanLabeling/Labelling_diff.py
@@ -38,7 +38,7 @@ class Labelling:
     def label(self,path,npz):
 
         data=dict(np.load(path+'/'+npz))
-        rdv=data['reduced_drums_velocity']>0
+        rdv=(data['reduced_drums_velocity']>0)*1
         diff=np.concatenate((rdv[:-2,:],rdv[1:-1],rdv[2:]),axis=1)
         y=((diff[:,1]-diff[:,0]).sum()>3 and (diff[:,2]-diff[:,1]).sum()>3)*1
         y=np.concatenate(([0],y,[0]))
