8ffd9acf33585feb9053ec580448a31a11794de8
ftamagnan <frederic.tamagnan@gmail.com>
2019-04-01 18:45:26 +0800

    training

--- sketchRnn_double/ScriptTrainingBeta.py
+++ sketchRnn_double/ScriptTrainingBeta.py
@@ -4,9 +4,9 @@ LR=0.001
 
 N_EPOCHS=300
 for beta in [250]:
-    for name,batch_size in [('Supervised',300)]:
+    for name,batch_size in [('Four',300)]:
         BATCH_SIZE=batch_size
-        local_dataset='/home/ftamagnan/dataset/FillsExtracted'+name+'07_cc.npz'
+        local_dataset='/home/ftamagnan/dataset/FillsExtracted'+name+'_cc.npz'
 
         tg=TrainingSketchRnn(lr=LR,batch_size=BATCH_SIZE,n_epochs=N_EPOCHS,dataset_filepath=local_dataset,beta=beta,linear_hidden_size=[64,32],gru_hidden_size=64)
         tg.load_data()
