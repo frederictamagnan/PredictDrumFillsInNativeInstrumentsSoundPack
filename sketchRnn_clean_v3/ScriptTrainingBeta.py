28d6a63b6b6eac081771da3d7b10442d7db06239
frederic tamagnan <frederic.tamagnan@gmail.com>
2019-04-02 10:45:09 +0800

    v3

--- sketchRnn_clean_v3/ScriptTrainingBeta.py
+++ sketchRnn_clean_v3/ScriptTrainingBeta.py
@@ -12,4 +12,4 @@ for beta in [250]:
         tg.load_data()
         tg.split_data()
         tg.train_model()
-        tg.save_model("./../models/",'sketchrnn_'+name+'_'+str(beta)+'_v2.pt')
+        tg.save_model("./../models/",'sketchrnn_'+name+'_'+str(beta)+'_v3.pt')
