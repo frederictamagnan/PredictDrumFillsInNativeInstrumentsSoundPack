7aed6fa018636f2d8a05402d67ed1b8d62f11ab3
frederic tamagnan <frederic.tamagnan@gmail.com>
2019-04-05 02:41:02 +0800

    modif train

--- PureRnn/ScriptTrainingBeta.py
+++ PureRnn/ScriptTrainingBeta.py
@@ -3,13 +3,14 @@ from TrainingSketchRnn import TrainingSketchRnn
 LR=0.001
 
 N_EPOCHS=300
-for beta in [250]:
-    for name,batch_size in [('Supervised',256)]:
-        BATCH_SIZE=batch_size
-        local_dataset='/home/ftamagnan/dataset/FillsExtracted'+name+'07_cc_cc.npz'
 
-        tg=TrainingSketchRnn(lr=LR,batch_size=BATCH_SIZE,n_epochs=N_EPOCHS,dataset_filepath=local_dataset,beta=beta,linear_hidden_size=[64,32],gru_hidden_size=64)
-        tg.load_data()
-        tg.split_data()
-        tg.train_model()
-        tg.save_model("./../models/",'sketchrnn_'+name+'_'+str(beta)+'_v4.pt')
+for name,batch_size in [('Supervised',256),('Supervised03',2048),('RuleBased',4096)]:
+    BATCH_SIZE=batch_size
+
+    local_dataset='/home/ftamagnan/dataset/FillsExtracted'+name+'_train.npz'
+
+    tg=TrainingSketchRnn(lr=LR,batch_size=BATCH_SIZE,n_epochs=N_EPOCHS,dataset_filepath=local_dataset,beta=0,linear_hidden_size=[64,32],gru_hidden_size=64)
+    tg.load_data()
+    tg.split_data()
+    tg.train_model()
+    tg.save_model("./../models/",'sketchrnn_'+name+'_v4.pt')
